2f0469b873d948188692efa03334bf9f213475e3
Mohammed Khaled <mohammedbeheiry95@gmail.com>
2021-02-08 15:48:40 +0200

    Note

--- TechHub_Task/src/main/java/com/example/TechHub_Task/TechHubTaskApplication.java
+++ TechHub_Task/src/main/java/com/example/TechHub_Task/TechHubTaskApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TechHubTaskApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TechHubTaskApplication.class, args);
+		 SpringApplication.run(TechHubTaskApplication.class, args);
 	}
 
 }
